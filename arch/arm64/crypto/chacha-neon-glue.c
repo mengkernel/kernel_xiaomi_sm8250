6633ecb22c52f99dc6e21290c714552364426416
Eric Biggers <ebiggers@google.com>
2019-03-12 22:12:50 -0700

    crypto: arm64 - convert to use crypto_simd_usable()
    Replace all calls to may_use_simd() in the arm64 crypto code with
    crypto_simd_usable(), in order to allow testing the no-SIMD code paths.
    
    Signed-off-by: Eric Biggers <ebiggers@google.com>
    Reviewed-by: Ard Biesheuvel <ard.biesheuvel@linaro.org>
    Signed-off-by: Herbert Xu <herbert@gondor.apana.org.au>
    [Diaz1401: adapt to v4.19]
    Signed-off-by: Diaz1401 <reagor8161@outlook.com>
    Conflicts:
      arch/arm64/crypto/chacha-neon-glue.c

--- arch/arm64/crypto/chacha-neon-glue.c
+++ arch/arm64/crypto/chacha-neon-glue.c
@@ -21,6 +21,7 @@
 
 #include <crypto/algapi.h>
 #include <crypto/internal/chacha.h>
+#include <crypto/internal/simd.h>
 #include <crypto/internal/skcipher.h>
 #include <linux/jump_label.h>
 #include <linux/kernel.h>
@@ -63,7 +64,7 @@ static void chacha_doneon(u32 *state, u8 *dst, const u8 *src,
 
 void hchacha_block_arch(const u32 *state, u32 *stream, int nrounds)
 {
-	if (!static_branch_likely(&have_neon) || !may_use_simd()) {
+	if (!static_branch_likely(&have_neon) || !crypto_simd_usable()) {
 		hchacha_block_generic(state, stream, nrounds);
 	} else {
 		kernel_neon_begin();
@@ -83,7 +84,7 @@ void chacha_crypt_arch(u32 *state, u8 *dst, const u8 *src, unsigned int bytes,
 		       int nrounds)
 {
 	if (!static_branch_likely(&have_neon) || bytes <= CHACHA_BLOCK_SIZE ||
-	    !may_use_simd())
+	    !crypto_simd_usable())
 		return chacha_crypt_generic(state, dst, src, bytes, nrounds);
 
 	do {
@@ -118,7 +119,7 @@ static int chacha_neon_stream_xor(struct skcipher_request *req,
 			nbytes = rounddown(nbytes, walk.stride);
 
 		if (!static_branch_likely(&have_neon) ||
-		    !may_use_simd()) {
+		    !crypto_simd_usable()) {
 			chacha_crypt_generic(state, walk.dst.virt.addr,
 					     walk.src.virt.addr, nbytes,
 					     ctx->nrounds);
--- arch/arm64/crypto/poly1305-glue.c
+++ arch/arm64/crypto/poly1305-glue.c
@@ -110,7 +110,7 @@ static void neon_poly1305_do_update(struct poly1305_desc_ctx *dctx,
 static int neon_poly1305_update(struct shash_desc *desc,
 				const u8 *src, unsigned int srclen)
 {
-	bool do_neon = may_use_simd() && srclen > 128;
+	bool do_neon = crypto_simd_usable() && srclen > 128;
 	struct poly1305_desc_ctx *dctx = shash_desc_ctx(desc);
 
 	if (static_branch_likely(&have_neon) && do_neon)
@@ -141,7 +141,7 @@ void poly1305_update_arch(struct poly1305_desc_ctx *dctx, const u8 *src,
 	if (likely(nbytes >= POLY1305_BLOCK_SIZE)) {
 		unsigned int len = round_down(nbytes, POLY1305_BLOCK_SIZE);
 
-		if (static_branch_likely(&have_neon) && may_use_simd()) {
+		if (static_branch_likely(&have_neon) && crypto_simd_usable()) {
 			do {
 				unsigned int todo = min_t(unsigned int, len, SZ_4K);
 
