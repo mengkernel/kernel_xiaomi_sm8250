772e75e2f75972a224c23859e04568a6e75bf19c
Martin Liu <liumartin@google.com>
2020-04-12 00:35:55 +0800

    mm: Revert previous mm revert list
    This commit reverts e799c1b10c54...cfb042c6c5d1
    
    Reason for revert: unblock GKI
    Bug: 140544941
    Test: boot
    Change-Id: I4ebe6c01918788cdc2468ceabf101ef7c3e3c452
    Signed-off-by: Martin Liu <liumartin@google.com>
    Signed-off-by: Diaz1401 <diaznura@outlook.com>

--- include/linux/mm_types.h
+++ include/linux/mm_types.h
@@ -336,11 +336,16 @@ struct vm_area_struct {
 	struct mempolicy *vm_policy;	/* NUMA policy for the VMA */
 #endif
 	struct vm_userfaultfd_ctx vm_userfaultfd_ctx;
+#ifdef CONFIG_SPECULATIVE_PAGE_FAULT
+	seqcount_t vm_sequence;
+	atomic_t vm_ref_count;		/* see vma_get(), vma_put() */
+#endif
 
 	ANDROID_KABI_RESERVE(1);
 	ANDROID_KABI_RESERVE(2);
 	ANDROID_KABI_RESERVE(3);
 	ANDROID_KABI_RESERVE(4);
+
 } __randomize_layout;
 
 struct core_thread {
@@ -360,6 +365,9 @@ struct mm_struct {
 		struct vm_area_struct *mmap;		/* list of VMAs */
 		struct rb_root mm_rb;
 		u64 vmacache_seqnum;                   /* per-thread vmacache */
+#ifdef CONFIG_SPECULATIVE_PAGE_FAULT
+		rwlock_t mm_rb_lock;
+#endif
 #ifdef CONFIG_MMU
 		unsigned long (*get_unmapped_area) (struct file *filp,
 				unsigned long addr, unsigned long len,
