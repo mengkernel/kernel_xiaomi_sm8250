90ab666079667e7758d84e6435a132fcaa1e443a
Jaegeuk Kim <jaegeuk@kernel.org>
2021-06-22 12:56:44 -0700

    f2fs: remove false alarm on iget failure during GC
    This patch removes setting SBI_NEED_FSCK when GC gets an error on f2fs_iget,
    since f2fs_iget can give ENOMEM and others by race condition.
    If we set this critical fsck flag, we'll get EIO during fsync via the below
    code path.
    
    In f2fs_inplace_write_data(),
    
    	if (is_sbi_flag_set(sbi, SBI_NEED_FSCK) || f2fs_cp_error(sbi)) {
    		err = -EIO;
    		goto drop_bio;
    	}
    
    Fixes: 9557727876674 ("f2fs: drop inplace IO if fs status is abnormal")
    Signed-off-by: Jaegeuk Kim <jaegeuk@kernel.org>
    
    Conflicts:
      fs/f2fs/gc.c
    Signed-off-by: Diaz1401 <diaznura@outlook.com>

--- fs/f2fs/gc.c
+++ fs/f2fs/gc.c
@@ -1457,10 +1457,8 @@ next_step:
 		if (phase == 3) {
 			inode = f2fs_iget(sb, dni.ino);
 			if (IS_ERR(inode) || is_bad_inode(inode) ||
-					special_file(inode->i_mode)) {
-				set_sbi_flag(sbi, SBI_NEED_FSCK);
+					special_file(inode->i_mode))
 				continue;
-			}
 
 			if (!down_write_trylock(
 				&F2FS_I(inode)->i_gc_rwsem[WRITE])) {
