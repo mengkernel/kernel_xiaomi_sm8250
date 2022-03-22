e1b87543bad28e62874824f1921c269589a04229
Diaz1401 <reagor8161@outlook.com>
2022-03-22 22:04:04 +0700

    treewide: Move some threads to big cluster
    Signed-off-by: Diaz1401 <reagor8161@outlook.com>

--- fs/exec.c
+++ fs/exec.c
@@ -1850,8 +1850,8 @@ static int __do_execve_file(int fd, struct filename *filename,
 		else if (unlikely(!strncmp(filename->name,
 					   HWCOMPOSER_BIN_PREFIX,
 					   strlen(HWCOMPOSER_BIN_PREFIX)))) {
-			current->pc_flags |= PC_PRIME_AFFINE;
-			set_cpus_allowed_ptr(current, cpu_prime_mask);
+			current->pc_flags |= PC_PERF_AFFINE;
+			set_cpus_allowed_ptr(current, cpu_perf_mask);
 		} else if (unlikely(!strncmp(filename->name,
 					   SURFACEFLINGER_BIN_PREFIX,
 					   strlen(SURFACEFLINGER_BIN_PREFIX)))) {
