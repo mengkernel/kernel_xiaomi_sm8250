d89b2ccdf99b25639a517a7cef46a362dd631a42
Diaz1401 <reagor8161@outlook.com>
2022-04-27 20:48:33 +0700

    arm64: lib: Import latest optimization
    * Imported from https://github.com/torvalds/linux/tree/0f61f6be1f7f44edfab0cb731c0a2340a838956f/arch/arm64/lib
    * Revert commit torvalds/linux@5f34b1e, torvalds/linux@ac0e8c7, torvalds/linux@742a15b
    * Also import required headers
    
    Signed-off-by: Diaz1401 <reagor8161@outlook.com>

--- arch/arm64/include/asm/linkage.h
+++ arch/arm64/include/asm/linkage.h
@@ -4,4 +4,45 @@
 #define __ALIGN		.align 2
 #define __ALIGN_STR	".align 2"
 
+#if defined(CONFIG_ARM64_BTI_KERNEL) && defined(__aarch64__)
+
+#define BTI_C bti c ;
+
+#else
+
+#define BTI_C
+
+#endif
+
+/*
+ * When using in-kernel BTI we need to ensure that PCS-conformant
+ * assembly functions have suitable annotations.  Override
+ * SYM_FUNC_START to insert a BTI landing pad at the start of
+ * everything, the override is done unconditionally so we're more
+ * likely to notice any drift from the overridden definitions.
+ */
+#define SYM_FUNC_START(name)				\
+	SYM_START(name, SYM_L_GLOBAL, SYM_A_ALIGN)	\
+	BTI_C
+
+#define SYM_FUNC_START_NOALIGN(name)			\
+	SYM_START(name, SYM_L_GLOBAL, SYM_A_NONE)	\
+	BTI_C
+
+#define SYM_FUNC_START_LOCAL(name)			\
+	SYM_START(name, SYM_L_LOCAL, SYM_A_ALIGN)	\
+	BTI_C
+
+#define SYM_FUNC_START_LOCAL_NOALIGN(name)		\
+	SYM_START(name, SYM_L_LOCAL, SYM_A_NONE)	\
+	BTI_C
+
+#define SYM_FUNC_START_WEAK(name)			\
+	SYM_START(name, SYM_L_WEAK, SYM_A_ALIGN)	\
+	BTI_C
+
+#define SYM_FUNC_START_WEAK_NOALIGN(name)		\
+	SYM_START(name, SYM_L_WEAK, SYM_A_NONE)		\
+	BTI_C
+
 #endif
