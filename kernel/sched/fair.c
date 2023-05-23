db1421ce3e7f2719cc36284c38f4c1f2dabb59b9
Diaz1401 <reagor8161@outlook.com>
2023-05-23 13:19:41 +0700

    Revert "kernel: sched: Mitigate non-boosted tasks preempting boosted tasks"
    This reverts commit 410d1db037c6a85c9dc4e7e95892082fd1fde50a.
    
    Signed-off-by: Diaz1401 <reagor8161@outlook.com>

--- kernel/sched/fair.c
+++ kernel/sched/fair.c
@@ -7165,7 +7165,6 @@ static void find_best_target(struct sched_domain *sd, cpumask_t *cpus,
 		prefer_idle = !!game_vip_task(p);
 #endif
 	int mid_cap_orig_cpu = cpu_rq(smp_processor_id())->rd->mid_cap_orig_cpu;
-	struct task_struct *curr_tsk;
 
 	/*
 	 * In most cases, target_capacity tracks capacity_orig of the most
@@ -7618,12 +7617,6 @@ static void find_best_target(struct sched_domain *sd, cpumask_t *cpus,
 	 *   a) ACTIVE CPU: target_cpu
 	 *   b) IDLE CPU: best_idle_cpu
 	 */
-	if (target_cpu != -1 && !idle_cpu(target_cpu) &&
-			best_idle_cpu != -1) {
-		curr_tsk = READ_ONCE(cpu_rq(target_cpu)->curr);
-		if (curr_tsk && schedtune_task_boost_rcu_locked(curr_tsk))
-			target_cpu = best_idle_cpu;
-	}
 
 	if (target_cpu == -1)
 		target_cpu = prefer_idle
