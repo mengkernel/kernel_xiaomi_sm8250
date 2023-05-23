3a8d265b33336d7f756ca4e9b293d303badfbfda
Diaz1401 <reagor8161@outlook.com>
2023-05-23 18:12:18 +0700

    dsp: q6adm: fix uninitialized variable warning
    ../include/linux/printk.h:311:9: error: 'port_idx' is used uninitialized [-Werror=uninitialized]
      311 |         printk(KERN_INFO pr_fmt(fmt), ##__VA_ARGS__)
          |         ^~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
    ../techpack/audio/dsp/q6adm.c:886:9: note: in expansion of macro 'pr_info'
      886 |         pr_info("[CSPL] %s: config: port_idx %d copp_idx  %d module 0x%d, len=%d\n",
          |         ^~~~~~~
    ../techpack/audio/dsp/q6adm.c:883:13: note: 'port_idx' was declared here
      883 |         int port_idx;
          |             ^~~~~~~~
    
    Signed-off-by: Diaz1401 <reagor8161@outlook.com>

--- techpack/audio/dsp/q6adm.c
+++ techpack/audio/dsp/q6adm.c
@@ -880,8 +880,8 @@ int crus_adm_set_params(int port_id, int copp_idx, uint32_t module_id,
 			 uint32_t params_length)
 {
 	struct param_hdr_v3 param_hdr;
-	int port_idx;
-	int rc  = 0;
+	int port_idx = 0;
+	int rc = 0;
 
 	pr_info("[CSPL] %s: config: port_idx %d copp_idx  %d module 0x%d, len=%d\n",
 			__func__, port_idx, copp_idx,module_id, params_length);
