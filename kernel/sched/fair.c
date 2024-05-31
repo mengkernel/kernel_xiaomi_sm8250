8747bf82ee771bab7d2a83e452a502f0e75a5fbf
Peter Zijlstra <peterz@infradead.org>
2024-05-31 15:49:40 +0200

    BACKPORT: sched/fair: Avoid re-setting virtual deadline on 'migrations'
    During OSPM24 Youssef noted that migrations are re-setting the virtual
    deadline. Notably everything that does a dequeue-enqueue, like setting
    nice, changing preferred numa-node, and a myriad of other random crap,
    will cause this to happen.
    
    This shouldn't be. Preserve the relative virtual deadline across such
    dequeue/enqueue cycles.
    
    Signed-off-by: Peter Zijlstra (Intel) <peterz@infradead.org>
    Reviewed-by: Valentin Schneider <vschneid@redhat.com>
    Tested-by: Valentin Schneider <vschneid@redhat.com>
    Link: https://lkml.kernel.org/r/20240727105030.625119246@infradead.org
    Signed-off-by: Diaz1401 <diaznura@outlook.com>
    
    Conflicts:
      include/linux/sched.h
      kernel/sched/fair.c

--- kernel/sched/fair.c
+++ kernel/sched/fair.c
@@ -4613,6 +4613,12 @@ place_entity(struct cfs_rq *cfs_rq, struct sched_entity *se, int flags)
 
 	se->vruntime = vruntime - lag;
 
+	if (sched_feat(PLACE_REL_DEADLINE) && se->rel_deadline) {
+		se->deadline += se->vruntime;
+		se->rel_deadline = 0;
+		return;
+	}
+
 	/*
 	 * When joining the competition; the exisiting tasks will be,
 	 * on average, halfway through their slice, as such start tasks
@@ -4729,6 +4735,7 @@ static __always_inline void return_cfs_rq_runtime(struct cfs_rq *cfs_rq);
 static void
 dequeue_entity(struct cfs_rq *cfs_rq, struct sched_entity *se, int flags)
 {
+	bool sleep = flags & DEQUEUE_SLEEP;
 	/*
 	 * Update run-time statistics of the 'current'.
 	 */
@@ -4750,6 +4757,11 @@ dequeue_entity(struct cfs_rq *cfs_rq, struct sched_entity *se, int flags)
 	clear_buddies(cfs_rq, se);
 
 	update_entity_lag(cfs_rq, se);
+	if (sched_feat(PLACE_REL_DEADLINE) && !sleep) {
+		se->deadline -= se->vruntime;
+		se->rel_deadline = 1;
+	}
+
 	if (se != cfs_rq->curr)
 		__dequeue_entity(cfs_rq, se);
 	se->on_rq = 0;
