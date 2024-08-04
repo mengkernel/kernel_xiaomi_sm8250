aaea6b7290618ecbbb61ea9d314acf6a7957dbe2
Diaz1401 <diaznura@outlook.com>
2024-08-04 13:11:15 +0000

    input: fts: Disable debug info & test
    Signed-off-by: Diaz1401 <diaznura@outlook.com>

--- drivers/input/touchscreen/focaltech_3658u/focaltech_config.h
+++ drivers/input/touchscreen/focaltech_3658u/focaltech_config.h
@@ -169,7 +169,7 @@
  * show debug log info
  * enable it for debug, disable it for release
  */
-#define FTS_DEBUG_EN 1
+#define FTS_DEBUG_EN 0
 
 /*
  * Linux MultiTouch Protocol
@@ -199,7 +199,7 @@
  * Production test enable
  * 1: enable, 0:disable(default)
  */
-#define FTS_TEST_EN 1
+#define FTS_TEST_EN 0
 
 /*
  * Pinctrl enable
--- drivers/input/touchscreen/focaltech_touch_mi/focaltech_config.h
+++ drivers/input/touchscreen/focaltech_touch_mi/focaltech_config.h
@@ -104,7 +104,7 @@
  * show debug log info
  * enable it for debug, disable it for release
  */
-#define FTS_DEBUG_EN 1
+#define FTS_DEBUG_EN 0
 
 /*
  * Linux MultiTouch Protocol
@@ -128,7 +128,7 @@
  * Production test enable
  * 1: enable, 0:disable(default)
  */
-#define FTS_TEST_EN 1
+#define FTS_TEST_EN 0
 
 /*
  * Glove mode enable
