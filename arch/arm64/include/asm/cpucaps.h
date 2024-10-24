30842dbe4afe9c4bd45501999aeabd9e0657bbcf
Diaz1401 <diaznura@outlook.com>
2024-10-24 20:00:25 +0700

    Revert "CHROMIUM: mm: multigenerational lru: add arch_has_hw_pte_young()"
    This reverts commit 218bbd5f2c26986a0ac42d022c917033fba3861e.

--- arch/arm64/include/asm/cpucaps.h
+++ arch/arm64/include/asm/cpucaps.h
@@ -57,12 +57,11 @@
 #define ARM64_WORKAROUND_1542419		37
 #define ARM64_SPECTRE_BHB			38
 #define ARM64_WORKAROUND_1742098		39
-#define ARM64_HW_AF				40
-#define ARM64_HAS_CRC32				41
-#define ARM64_HAS_SB				42
-#define ARM64_WORKAROUND_SPECULATIVE_SSBS	43
+#define ARM64_HAS_CRC32				40
+#define ARM64_HAS_SB				41
+#define ARM64_WORKAROUND_SPECULATIVE_SSBS	42
 
-/* kabi: reserve 44 - 62 for future cpu capabilities */
+/* kabi: reserve 43 - 62 for future cpu capabilities */
 #define ARM64_NCAPS				62
 
 #endif /* __ASM_CPUCAPS_H */
