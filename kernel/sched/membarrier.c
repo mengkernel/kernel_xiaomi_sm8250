0be8943318b56da327f3cf617033b1a8368105a0
Diaz1401 <diaznura@outlook.com>
2025-02-26 15:58:52 +0700

    pre-checkout
    This reverts commit:
    65c1957181a1 kthread: add kthread_work tracepoints
    2890a7faf552 mm: optimize the redundant loop of mm_update_owner_next()
    454de5ed8176 sched/fair: Allow disabling sched_balance_newidle with sched_relax_domain_level
    046daa54c348 sched/topology: Don't set SD_BALANCE_WAKE on cpuset domain relax
    260d03bf3676 sched: Provide sched_set_fifo()
    2d931472d474 sched/rt: Disallow writing invalid values to sched_rt_period_us
    1f80bc015277 sched/rt: sysctl_sched_rr_timeslice show default timeslice after reset
    41b7572dea9f sched/rt: Fix sysctl_sched_rr_timeslice intial value
    3cd139875e9a sched/membarrier: reduce the ability to hammer on sys_membarrier
    853e029bb25f UPSTREAM: sched/psi: Fix use-after-free in ep_remove_wait_queue()
    25de4daf4eaf UPSTREAM: sched/fair: Fix cfs_rq_clock_pelt() for throttled cfs_rq
    bcbcb0eaec98 UPSTREAM: copy_process(): Move fd_install() out of sighand->siglock critical section
    f69b5bfb7de8 UPSTREAM: psi: Fix "no previous prototype" warnings when CONFIG_CGROUPS=n
    27065f2677eb UPSTREAM: sched/uclamp: Fix rq->uclamp_max not set on first enqueue
    d3029b586e2e UPSTREAM: sched/uclamp: Fix uclamp_tg_restrict()
    16187c3b2d36 UPSTREAM: sched/uclamp: Fix a deadlock when enabling uclamp static key
    97ac2589ac95 UPSTREAM: fork: return proper negative error code
    788621afda41 sched,idle,rcu: Push rcu_idle deeper into the idle path
    
    Signed-off-by: Diaz1401 <diaznura@outlook.com>

--- kernel/sched/membarrier.c
+++ kernel/sched/membarrier.c
@@ -34,8 +34,6 @@
 	| MEMBARRIER_CMD_REGISTER_PRIVATE_EXPEDITED			\
 	| MEMBARRIER_PRIVATE_EXPEDITED_SYNC_CORE_BITMASK)
 
-static DEFINE_MUTEX(membarrier_ipi_mutex);
-
 static void ipi_mb(void *info)
 {
 	smp_mb();	/* IPIs should be serializing but paranoid. */
@@ -66,7 +64,6 @@ static int membarrier_global_expedited(void)
 		fallback = true;
 	}
 
-	mutex_lock(&membarrier_ipi_mutex);
 	cpus_read_lock();
 	for_each_online_cpu(cpu) {
 		struct task_struct *p;
@@ -107,7 +104,6 @@ static int membarrier_global_expedited(void)
 	 * rq->curr modification in scheduler.
 	 */
 	smp_mb();	/* exit from system call is not a mb */
-	mutex_unlock(&membarrier_ipi_mutex);
 	return 0;
 }
 
@@ -148,7 +144,6 @@ static int membarrier_private_expedited(int flags)
 		fallback = true;
 	}
 
-	mutex_lock(&membarrier_ipi_mutex);
 	cpus_read_lock();
 	for_each_online_cpu(cpu) {
 		struct task_struct *p;
@@ -187,7 +182,6 @@ static int membarrier_private_expedited(int flags)
 	 * rq->curr modification in scheduler.
 	 */
 	smp_mb();	/* exit from system call is not a mb */
-	mutex_unlock(&membarrier_ipi_mutex);
 
 	return 0;
 }
