6b6a7ebcf7821d95e1f3ee0e5851bee926717a32
Diaz1401 <diaznura@outlook.com>
2025-07-18 12:39:05 +0700

    fixup! f2fs: fix to do sanity check on sbi->total_valid_block_count
    We already use `f2fs_warn()` in commit mengkernel/kernel_xiaomi_alioth@ae8ba98776eb959dab985fdf48d07aca6a40db4c.
    Fix commit mengkernel/kernel_xiaomi_alioth@5f77f83223afdc36a1eea6bfed7788e2bbb18ac6.
    
    Signed-off-by: Diaz1401 <diaznura@outlook.com>

--- fs/f2fs/f2fs.h
+++ fs/f2fs/f2fs.h
@@ -2091,9 +2091,8 @@ static inline void dec_valid_block_count(struct f2fs_sb_info *sbi,
 
 	spin_lock(&sbi->stat_lock);
 	if (unlikely(sbi->total_valid_block_count < count)) {
-		f2fs_msg(sbi->sb, KERN_WARNING,
-			 "Inconsistent total_valid_block_count:%u, ino:%lu, count:%u",
-			 sbi->total_valid_block_count, inode->i_ino, count);
+		f2fs_warn(sbi, "Inconsistent total_valid_block_count:%u, ino:%lu, count:%u",
+			  sbi->total_valid_block_count, inode->i_ino, count);
 		sbi->total_valid_block_count = 0;
 		set_sbi_flag(sbi, SBI_NEED_FSCK);
 	} else {
