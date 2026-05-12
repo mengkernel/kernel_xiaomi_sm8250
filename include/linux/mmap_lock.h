bcc1f8c495678adbe965a5a8cd2197c0b2973392
d1aznr <diaznura@outlook.com>
2026-05-12 22:12:14 +0700

    fixup! BACKPORT: mmap locking API: use coccinelle to convert mmap_sem rwsem call sites
    dumb mistake, fixes 3e77eabdc4e24db2ab94d36f58da46f55abeb8d7

--- include/linux/mmap_lock.h
+++ include/linux/mmap_lock.h
@@ -3,52 +3,52 @@
 
 static inline void mmap_init_lock(struct mm_struct *mm)
 {
-	mmap_init_lock(mm);
+	init_rwsem(&mm->mmap_sem);
 }
 
 static inline void mmap_write_lock(struct mm_struct *mm)
 {
-	mmap_write_lock(mm);
+	down_write(&mm->mmap_sem);
 }
 
 static inline int mmap_write_lock_killable(struct mm_struct *mm)
 {
-	return mmap_write_lock_killable(mm);
+	return down_write_killable(&mm->mmap_sem);
 }
 
 static inline bool mmap_write_trylock(struct mm_struct *mm)
 {
-	return mmap_write_trylock(mm) != 0;
+	return down_write_trylock(&mm->mmap_sem) != 0;
 }
 
 static inline void mmap_write_unlock(struct mm_struct *mm)
 {
-	mmap_write_unlock(mm);
+	up_write(&mm->mmap_sem);
 }
 
 static inline void mmap_write_downgrade(struct mm_struct *mm)
 {
-	mmap_write_downgrade(mm);
+	downgrade_write(&mm->mmap_sem);
 }
 
 static inline void mmap_read_lock(struct mm_struct *mm)
 {
-	mmap_read_lock(mm);
+	down_read(&mm->mmap_sem);
 }
 
 static inline int mmap_read_lock_killable(struct mm_struct *mm)
 {
-	return mmap_read_lock_killable(mm);
+	return down_read_killable(&mm->mmap_sem);
 }
 
 static inline bool mmap_read_trylock(struct mm_struct *mm)
 {
-	return mmap_read_trylock(mm) != 0;
+	return down_read_trylock(&mm->mmap_sem) != 0;
 }
 
 static inline void mmap_read_unlock(struct mm_struct *mm)
 {
-	mmap_read_unlock(mm);
+	up_read(&mm->mmap_sem);
 }
 
 static inline bool mmap_read_trylock_non_owner(struct mm_struct *mm)
